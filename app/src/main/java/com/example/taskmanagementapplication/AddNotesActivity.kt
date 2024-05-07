3b654eecbbf35d004dad077ec26679df7db4e6cf
Christy <christy.tck@gmail.com>
2024-05-07 23:36:43 +0530

    2nd commit

--- app/src/main/java/com/example/taskmanagementapplication/AddNotesActivity.kt
+++ app/src/main/java/com/example/taskmanagementapplication/AddNotesActivity.kt
@@ -1,20 +1,43 @@
 package com.example.taskmanagementapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.taskmanagementapplication.databinding.ActivityAddNotesBinding
+import com.example.taskmanagementapplication.databinding.ActivityMainBinding
 
 class AddNotesActivity : AppCompatActivity() {
+
+    //Setting up Binding
+    private lateinit var binding: ActivityAddNotesBinding
+
+    private lateinit var db: NoteDatabaseHelper
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_notes)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityAddNotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //initializing on database
+
+        db = NoteDatabaseHelper(this)
+
+        //Getting User Inputs
+
+        binding.saveBtn.setOnClickListener{
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0,title,content)
+            db.insertNote(note) //Inserting values into the database
+            finish() //Close the Activity
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
         }
+
     }
 }
